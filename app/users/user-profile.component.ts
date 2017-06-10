49ea39aa427d07e64f51b380c34d435eeb6297c9
wouzar <wouzar@gmail.com>
2017-06-10 19:53:46 +0300

    Create user component

---
+++ app/users/user-profile.component.ts
@@ -0,0 +1,19 @@
+/**
+ * Created by wouzar on 10.06.17.
+ */
+import { Component, Input } from '@angular/core';
+import { User } from '../shared/models/user';
+
+@Component({
+    selector: 'user-profile',
+    template: `
+        <div class="jumbotron" *ngIf="user">
+            <h2>{{ user.name }} <small>{{ user.username }}</small></h2>
+
+            <input class="form-control" [(ngModel)]="user.name">
+        </div>
+    `
+})
+export class UserProfileComponent {
+    @Input() user: User;
+}
