99e30ee102ea3d56bd7054291b62ae1f80f0ae2d
wouzar <wouzar@gmail.com>
2017-06-10 19:30:27 +0300

    Make cleanup, add users rendering

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,17 +2,76 @@
  * Created by wouzar on 10.06.17.
  */
 import { Component } from '@angular/core';
+import { User } from './shared/models/user';
 
 @Component({
     selector: 'my-app',
     template: `
-        <div class="jumbotron">
-            <h1>Welcome to Our App!</h1>
-        </div>
+        <header>
+            <nav class="navbar navbar-inverse">
+                <div class="navbar-header">
+                    <a href="/" class="navbar-brand">My Angular App!</a>
+                </div>
+            </nav>
+        </header>
+        
+        <main>
+            <div class="row">
+
+                <div class="col-sm-4">
+                    <div *ngIf="users">
+                        <ul class="list-group users-list">
+                            <li class="list-group-item" 
+                                *ngFor="let user of users"
+                                (click)="selectUser(user)"
+                                [class.active]="user === activeUser">
+                                {{ user.name }} ({{ user.username}})
+                            </li>
+                        </ul>
+                    </div>        
+                </div>
+                
+                <div class="col-sm-8">
+                    <div class="jumbotron" *ngIf="activeUser">
+                        <h2>{{ activeUser.name }} <small>{{ activeUser.username }}</small></h2>
+                    </div>
+                    
+                    <div class="jumbotron" *ngIf="!activeUser">
+                        <span class="glyphicon glyphicon-hand-left"></span>
+                        <h2>Choose a User</h2>
+                    </div>
+                </div>
+                
+            </div>
+        </main>
+        
+        <footer class="text-center">
+            Copyright &copy; 2016
+        </footer>
     `,
+
     styles: [`
-        .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }
+        .users-list li {
+            cursor: pointer;
+        }
+        .jumbotron .glyphicon {
+            font-size: 80px;
+        }
     `]
 
 })
-export class AppComponent {}
+export class AppComponent {
+    message = 'Hello!';
+    users: User[] = [
+        { id: 25, name: 'Victor', username: 'wouzar' },
+        { id: 26, name: 'Nastya', username: 'nastya' },
+        {id : 27, name: 'Pall', username: 'calpall'}
+    ];
+    activeUser: User;
+
+    selectUser(user) {
+        this.activeUser = user;
+        console.log(this.activeUser);
+    }
+
+}
---
+++ app/shared/models/user.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by wouzar on 10.06.17.
+ */
+export class User {
+    id: number;
+    name: string;
+    username: string;
+}
