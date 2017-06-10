37edab525182e8241332c191d3acdb26df6a9004
wouzar <wouzar@gmail.com>
2017-06-10 20:05:41 +0300

    Create user form

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,13 +6,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './users/user-profile.component';
+import { UserFormComponent } from './users/user-form.component';
 
 @NgModule ({
     imports: [
         BrowserModule,
         FormsModule
     ],
-    declarations: [ AppComponent, UserProfileComponent ],
+    declarations: [
+        AppComponent,
+        UserProfileComponent,
+        UserFormComponent
+    ],
     bootstrap: [ AppComponent ]
 })
 export class AppModule {}
